091698e46ff718cfe366b360a0e80bea15dccd2f
Codies | Marcus S <43828282+tbfCodies@users.noreply.github.com>
2024-03-21 19:31:23 +0100

    Add files via upload

--- events/start/ready.js
+++ events/start/ready.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const { connectDB } = require("../../utils/database/databaseHandler.js");
 
 module.exports = {
     name: "ready",
@@ -19,10 +18,6 @@ module.exports = {
         })
 
         try {
-
-            // Connect to the database
-            connectDB();
-
             // REMOVE THIS IF ONLY ONE GUILD/SERVER
             const guilds = client.guilds.cache;
             guilds.forEach(async guild => {
